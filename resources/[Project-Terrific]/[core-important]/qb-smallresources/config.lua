c682b07c6bd9765ddce3d6c66c5a55f9ff77c471
Dev Terrific <119811229+devterrific@users.noreply.github.com>
2023-11-05 23:44:16 +0100

    Update config.lua

--- resources/[Project-Terrific]/[core-important]/qb-smallresources/config.lua
+++ resources/[Project-Terrific]/[core-important]/qb-smallresources/config.lua
@@ -93,7 +93,7 @@ Config.DisableAmbience = true -- Disabled distance sirens, distance car alarms, 
 Config.HarnessUses = 20
 Config.DamageNeeded = 100.0 -- vehiclepush 0-1000
 Config.EnableProne = false -- prone isnt recomended at this time
-Config.MapText = "Project Phoenix" -- This is the name / text shown above the map
+Config.MapText = "Project Terrific " -- This is the name / text shown above the map
 
 Config.Disable = {
     disableHudComponents = {1, 2, 3, 4, 7, 9, 13, 19, 20, 21, 22}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
